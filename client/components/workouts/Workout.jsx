947459035eca97a0acb03401ae545d5fc90314b0
jhernandez504 <Jeremy.hernandez504@gmail.com>
2024-10-25 11:00:14 -0500

    Feat/ workout components' render skeleton initialized

--- client/components/workouts/Workout.jsx
+++ client/components/workouts/Workout.jsx
@@ -1,4 +1,16 @@
+import React from 'react';
+
 //render individual workout
 
 //receives routine as a prop from WorkoutList.jsx
+const Workout = () => {
 
+
+  render() {
+    return (
+      <div></div>
+    )
+  };
+};
+
+export default Workout;
--- client/components/workouts/WorkoutSearch.jsx
+++ client/components/workouts/WorkoutSearch.jsx
@@ -1,5 +1,38 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
 //should have search bar
 
 //populates result window with results
 
 //click on results to add to saved workout list
+const WorkoutSearch = () => {
+  //search input state
+  const [searchQuery, setSearchQuery] = useState('');
+
+  //inputChange handler to update searchQuery state on keystroke change
+  const handleInputChange = (e) => {
+    setSearchQuery(e.target.value);
+  }
+
+  return (
+    <div>
+      <form action="/search-workouts" method="GET">
+        <label for="query">Search Workouts:</label>
+        <input
+          type="search"
+          id="query"
+          name="query"
+          placeholder="Type workout name..."
+          required
+          onChange={handleInputChange}
+          value={searchQuery}
+        />
+        <button type="submit">Search</button>
+      </form>
+    </div>
+  )
+
+};
+
+export default WorkoutSearch;
